f4994fe94358737316a6d78f087790859f4b33ca
merothh <merothh@gmail.com>
2020-10-12 22:46:37 +0530

    blocks.h: tweak date block

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/* Icon      Command                         Interval	Signal */
+	{  " ",     "date '+%a %Y-%m-%d %I:%M %p'", 30,        0         },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
