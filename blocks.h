f20ed83e0660690c3762341fdf885c858bbabdde
merothh <merothh@gmail.com>
2020-12-09 14:24:03 +0530

    blocks.h: add a brightness block

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Icon      Command                         Interval	Signal */
+	{  "",       "brightness",                   30,        3         },
 	{  "",       "net",                          30,        2         },
 	{  "",       "battery",                      60,        1         },
 	{  " ",     "date '+%a %Y-%m-%d %I:%M %p'", 30,        0         },
