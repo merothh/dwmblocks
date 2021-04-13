8c45f7f9b50cfd3101c0e7c9d9baf457ca007bd4
merothh <merothh@gmail.com>
2021-04-13 13:37:31 +0530

    blocks.h: add a spotify block

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Icon      Command                         Interval	Signal */
+	{  "",       "spotify-blk",                  10,        5         },
 	{  "",       "volume",                       30,        4         },
 	{  "",       "brightness",                   30,        3         },
 	{  "",       "net",                          30,        2         },
